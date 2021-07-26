6e128b3756e09a835f4ea2160b88c6707ca154d1
Yana <yanahill29@gmail.com>
2021-07-26 13:38:16 +0300

    Exclude BeanE with filter

--- Spring_1_IntroNCore/src/main/java/com/lab/spring/intro/config/Conf2.java
+++ Spring_1_IntroNCore/src/main/java/com/lab/spring/intro/config/Conf2.java
@@ -1,21 +1,12 @@
 package com.lab.spring.intro.config;
 
-import com.lab.spring.intro.beans3.BeanD;
-import com.lab.spring.intro.beans3.BeanF;
-import org.springframework.context.annotation.Bean;
+import com.lab.spring.intro.beans3.BeanE;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
 
 @Configuration
-@ComponentScan(basePackages = "com.lab.spring.intro.beans2")
+@ComponentScan(basePackages = {"com.lab.spring.intro.beans2", "com.lab.spring.intro.beans3"},
+        excludeFilters = @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = BeanE.class))
 public class Conf2 {
-    @Bean
-    public BeanD beanD() {
-        return new BeanD();
-    }
-
-    @Bean
-    public BeanF beanF() {
-        return new BeanF();
-    }
 }
