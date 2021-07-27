797d3e0864ab928bc9f4b4c5d0ca100069e093b7
Yana <yanahill29@gmail.com>
2021-07-27 12:41:13 +0300

    Add postConstruct and preDestroy methods

--- Spring_2_Core/src/main/java/com/epam/spring/core/beans/BeanE.java
+++ Spring_2_Core/src/main/java/com/epam/spring/core/beans/BeanE.java
@@ -1,7 +1,12 @@
 package com.epam.spring.core.beans;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+@Component
 public class BeanE {
     private String name;
     private String value;
@@ -23,5 +28,15 @@ public class BeanE {
     @Override
     public String toString() {
         return "[E:" + name + "->" + value + ']';
+    }
+
+    @PostConstruct
+    public void postConstruct() {
+        System.out.println(this + ".postConstruct()");
+    }
+
+    @PreDestroy
+    public void preDestroy() {
+        System.out.println(this + ".preDestroy()");
     }
 }
