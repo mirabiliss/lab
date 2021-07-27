be7f32ec222660969e9d1e86239b3d075ee9a888
Yana <yanahill29@gmail.com>
2021-07-27 15:19:50 +0300

    Clarify output

--- Spring_2_Core/src/main/java/com/epam/spring/core/Demo.java
+++ Spring_2_Core/src/main/java/com/epam/spring/core/Demo.java
@@ -5,12 +5,15 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 
 public class Demo {
     public static void main(String[] args) {
+        System.out.println(System.lineSeparator() + "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+                + System.lineSeparator() + "\tStarting application context" + System.lineSeparator()
+                + "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~" + System.lineSeparator());
         AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(Config1.class);
-
         System.out.println(System.lineSeparator() + "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
                 + System.lineSeparator() + "\tApplication context is ready" + System.lineSeparator()
                 + "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~" + System.lineSeparator());
 
+        System.out.println("Bean definition names:" + System.lineSeparator());
         for (String name : context.getBeanDefinitionNames()) {
             System.out.println(name);
         }
